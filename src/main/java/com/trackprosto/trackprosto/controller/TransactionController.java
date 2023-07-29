0f80f509f3d839a7c6d10d3bd7cacbcbdc92a7f3
robysetyaw <robysetyaw@gmail.com>
2023-07-29 16:16:12 +0700

    change transaction list response

--- src/main/java/com/trackprosto/trackprosto/controller/TransactionController.java
+++ src/main/java/com/trackprosto/trackprosto/controller/TransactionController.java
@@ -2,7 +2,9 @@ package com.trackprosto.trackprosto.controller;
 
 
 import com.trackprosto.trackprosto.model.entity.Transaction;
+import com.trackprosto.trackprosto.model.request.CustomerRequest;
 import com.trackprosto.trackprosto.model.request.TransactionRequest;
+import com.trackprosto.trackprosto.model.response.TemplateResponse;
 import com.trackprosto.trackprosto.service.TransactionService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -22,8 +24,11 @@ public class TransactionController {
     }
 
     @GetMapping
-    public List<Transaction> findAll() {
-        return transactionService.findAll();
+    public TemplateResponse<List<Transaction>> findAll() {
+        TemplateResponse<List<Transaction>> res = new TemplateResponse<List<Transaction>>();
+        res.message = "success";
+        res.data = transactionService.findAll();
+        return res;
     }
 
     @GetMapping("/{id}")
