4da96c1caa27938e9a94cec994505a088090ebed
robysetyaw <robysetyaw@gmail.com>
2023-07-30 12:33:29 +0700

    add Validation to CustomerRequest

--- src/main/java/com/trackprosto/trackprosto/model/request/CustomerRequest.java
+++ src/main/java/com/trackprosto/trackprosto/model/request/CustomerRequest.java
@@ -1,6 +1,7 @@
 package com.trackprosto.trackprosto.model.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ public class CustomerRequest {
     private String address;
 
     @NotBlank
+    @Pattern(regexp = "^\\d+$", message = "Phone number must only contain numbers")
     private String phoneNumber;
 
     @NotBlank
