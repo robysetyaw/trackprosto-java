0624c6d17c5466aff8ef859bd613f57815220cb7
robysetyaw <robysetyaw@gmail.com>
2023-07-30 11:37:53 +0700

    add CreditPayment Controller

---
+++ src/main/java/com/trackprosto/trackprosto/controller/CreditPaymentController.java
@@ -0,0 +1,31 @@
+package com.trackprosto.trackprosto.controller;
+
+import com.trackprosto.trackprosto.model.entity.CreditPayment;
+import com.trackprosto.trackprosto.service.CreditPaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/credit-payments")
+public class CreditPaymentController {
+
+    private final CreditPaymentService creditPaymentService;
+
+    @Autowired
+    public CreditPaymentController(CreditPaymentService creditPaymentService) {
+        this.creditPaymentService = creditPaymentService;
+    }
+
+    @PostMapping
+    public CreditPayment save(@RequestBody CreditPayment creditPayment) {
+        return creditPaymentService.save(creditPayment);
+    }
+
+    @GetMapping("/invoice/{invNumber}")
+    public List<CreditPayment> findByInvNumber(@PathVariable String invNumber) {
+        return creditPaymentService.findByInvNumber(invNumber);
+    }
+
+}
